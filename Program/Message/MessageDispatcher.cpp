3c408935af3f55c3c94704461905e4990913f988
Risaki <kyozyu_masa@yahoo.co.jp>
2021-12-30 02:48:27 +0900

    不要な定数を削除

--- Program/Message/MessageDispatcher.cpp
+++ Program/Message/MessageDispatcher.cpp
@@ -12,8 +12,7 @@ bool MessageDispatcher::Dispatch() const
 
     while ( PeekMessage( &info, nullptr, 0, 0, PM_REMOVE ) != 0 )
     {
-        const auto IS_QUIT = info.message == WM_QUIT;
-        if ( IS_QUIT ) return false;
+        if ( info.message == WM_QUIT ) return false;
 
         ::TranslateMessage( &info );
         DispatchMessage( &info );
